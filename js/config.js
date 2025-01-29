b7e3f9dc37fee809801b401e43ec3606f06c2cb2
jakeschramm <82625035+jakeschramm@users.noreply.github.com>
2025-01-29 10:17:38 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_4g1ex00sh', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7i8p1435k8k4s8bdb2343i8jbc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
