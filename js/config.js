1f1d3ae4b6cf5c8e18b5e550133f8caf814cc2b2
jakeschramm <82625035+jakeschramm@users.noreply.github.com>
2025-01-29 10:29:09 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_4g1ex00sh', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7i8p1435k8k4s8bdb2343i8jbc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_tpn6E9zrt', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5mga13773rkupb991p2o71ugqg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
